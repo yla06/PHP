85e4ab9a41ae4602bded9a28025c3f4c6403032b
yla06 <33964687+yla06@users.noreply.github.com>
2019-05-19 01:47:50 +0300

    Create index.php

---
+++ shop/index.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<!DOCTYPE>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<style type="text/css">
+		body {
+			margin: 0;
+			padding: 0;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="header">
+			<a href="http://st411.phpstep.com.ua/shop/register.php">Реєстрація</a>
+			<a href="http://st411.phpstep.com.ua/shop/authorize.php">Увійти</a>
+		</div>
+	</body>
+</html>
