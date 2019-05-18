cf4b2a74577cca6cf12833494ace824cbdb7bcb3
yla06 <33964687+yla06@users.noreply.github.com>
2019-05-19 01:17:29 +0300

    Create view.php

---
+++ blog_file/view.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'bootstrap.php';
+
+$data = unserialize( file_get_contents('blog.dat'));
+
+$key =(isset( $_GET['key']) and is_string( $_GET['key'])) ? $_GET['key'] : false;
+
+if( false === $key or ! isset( $data[$key]))
+    exit('Ключ не найден');
+?>
+
+
+<h1><?= htmlspecialchars( $data[$key]['title']) ?></h1>
+<div><?= htmlspecialchars( $data[$key]['text']) ?></div>
+
