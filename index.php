61049fa72e7ba987dbc533202848166a3e384593
yla06 <33964687+yla06@users.noreply.github.com>
2019-05-19 02:09:16 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+/* echo 'Hello world';
+//echo 5;
+# echo 7;*/
+/* 
+ echo 7;
+ */
+/*echo 12;//comment
+echo 13;*/
+//echo 2/* comennt*/ //+2; */
+
+// ________Zminni
+
+/*
+$foo=rand(1,15);
+$bar=2+2;
+echo $foo;
+echo$foo+$bar;
+ */
+
+//echo $baz;
+/*
+$foo=2;
+$bar=&$foo;
+echo $bar;//2
+$bar=17;
+echo $foo;//17
+unset($foo);*/
+
+//константи
+
+/*define('FOO', 1);//константа з назвою FOO і знач 1
+$Foo=2;
+echo $Foo; //2
+$Foo=3;
+echo $Foo; //3
+echo FOO;
+define ('foo', 7);
+echo FOO;*/
+
+//зарезервовані константи
+
+/*echo __file__;
+echo __line__;*/
+
+//оператори
+
+//echo 7%3; //1
+
+echo 6%11; //23
+$foo=2;
+$bar=3;
+echo $foo.$bar;//23
+$foo='Hello';
+echo $foo.'world';
+
+
