8ae98b7a617df01290e83356e773c880c934cc3b
yla06 <33964687+yla06@users.noreply.github.com>
2019-05-19 01:34:45 +0300

    Create dz5.php

---
+++ dz/dz5.php
@@ -0,0 +1,55 @@
+<?php
+/*
+function foo(){
+    echo 5 . '<br />';
+}
+
+foo();
+foo();
+
+function bar(){
+    return 3 . '<br />';
+}
+echo bar();
+echo bar();
+echo '<br />';
+
+function divide($a, $b=1) {
+    if($b == 0) echo 'на нуль ділити не можна';
+    else echo $a/$b.'<br />'; 
+}
+
+divide(22, 11);
+divide(-33, -2);
+divide(3);
+divide(11, 0);
+
+function even_numbers($c, $d) {
+    for( $i=$c; $i<$d; $i++) {
+        if($i%2 == 0) {
+            $arr[$i]= array();
+            
+        }
+        
+    }
+    echo count($arr);
+}
+
+even_numbers(3, 7);//2
+*/
+
+/*
+$arr = array(1, 2, 3);
+unset($arr[0]);
+echo $arr[2];
+
+$arr1 = [
+    'one'=>[1, 2, 3],
+    'two'=>[4, 5, 6],
+    3 => [7, 8, 9],
+    4 => [10, 11, 12]
+];
+unset ($arr1['one'][0]);
+echo $arr1[4][2];
+if ( !(2-2+"hello") ){echo 1;} 
+*/
